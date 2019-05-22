02773906958448d6d20cfabaf72d4e7f05ec343b
wfxronald <cool.starmon@gmail.com>
2019-05-22 17:31:03 +0800

    implement user database and user login

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateField
 from wtforms.validators import DataRequired
 
 
@@ -8,3 +8,11 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class MockForm(FlaskForm):
+    username = StringField('Name of Staff', validators=[DataRequired()])
+    userid = StringField('Staff ID', validators=[DataRequired()])
+    designation = StringField('Designation', validators=[DataRequired()])
+    date = DateField('Date Appealed (in "YYYY-MM-DD" format)', validators=[DataRequired()])
+    submit = SubmitField('Submit Form')
--- main/routes.py
+++ main/routes.py
@@ -1,19 +1,48 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from main import app
-from main.forms import LoginForm
+from main.forms import LoginForm, MockForm
+from flask_login import current_user, login_user, logout_user, login_required
+from main.models import User
+from werkzeug.urls import url_parse
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+@login_required
 def index():
-    user = {'username': 'Admin'}
-    return render_template('index.html', title='Home', user=user)
+    form = MockForm()
+    if form.validate_on_submit():
+        flash("Data format is correct, but have not been stored yet")
+        return redirect(url_for('index'))
+    return render_template('index.html', title='Home', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # If already logged in, cannot login anymore
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        flash("Login functionality not implemented yet. Sorry, {}".format(form.username.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+
+        # Check if user has been registered and if the password matches
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+
+        next_page = request.args.get('next')
+        # Redirect to index if full URL is included to make the application more secure
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
