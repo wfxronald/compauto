68b36af97c5e57fc80109862391ceb33b6b11d5b
wfxronald <cool.starmon@gmail.com>
2019-06-03 10:28:31 +0800

    change database structure to follow excel sheet

--- main/forms.py
+++ main/forms.py
@@ -11,21 +11,12 @@ class LoginForm(FlaskForm):
 
 
 class MainForm(FlaskForm):
-    crm_app_no = StringField('CRM Application Number', validators=[DataRequired()])
-    fna_no = StringField('FNA Number', validators=[DataRequired()])
-
-    created_by_name = SelectField('Name of Staff', coerce=str, validators=[DataRequired()])
-    created_by_id = SelectField('Staff ID', coerce=str, validators=[DataRequired()])
-    create_date = DateField('Date Created in YYYY-MM-DD format', validators=[DataRequired()])
-
     closed_by_name = SelectField('Name of Staff', coerce=str, validators=[DataRequired()])
     closed_by_id = SelectField('Staff ID', coerce=str, validators=[DataRequired()])
     close_date = DateField('Date Closed in YYYY-MM-DD format', validators=[DataRequired()])
 
     assign_to_name = SelectField('Name of Staff', coerce=str, validators=[DataRequired()])
     assign_to_id = SelectField('Staff ID', coerce=str, validators=[DataRequired()])
-
-    pdt_name = StringField('Product Name', validators=[DataRequired()])
 
     submit = SubmitField('Submit Form')
 
--- main/routes.py
+++ main/routes.py
@@ -14,26 +14,22 @@ from flask_table import Table, Col
 def index():
     form = MainForm()
 
-    form.created_by_name.choices = [(g.staff_name, g.staff_name) for g in User.query.order_by('staff_name')]
     form.closed_by_name.choices = [(g.staff_name, g.staff_name) for g in User.query.order_by('staff_name')]
     form.assign_to_name.choices = [(g.staff_name, g.staff_name) for g in User.query.order_by('staff_name')]
 
     # Maintain consistent ordering with the choices for staff name
-    form.created_by_id.choices = [(g.staff_id, g.staff_id) for g in User.query.order_by('staff_name')]
     form.closed_by_id.choices = [(g.staff_id, g.staff_id) for g in User.query.order_by('staff_name')]
     form.assign_to_id.choices = [(g.staff_id, g.staff_id) for g in User.query.order_by('staff_name')]
 
     def add_empty_name(choices):
         choices.insert(0, ("", "<Please Select>"))  # The empty option is the first option, i.e. default option
 
-    add_empty_name(form.created_by_name.choices)
     add_empty_name(form.closed_by_name.choices)
     add_empty_name(form.assign_to_name.choices)
 
     def add_empty_id(choices):
         choices.insert(0, ("", "<Select Name Above>"))  # The empty option is the first option, i.e. default option
 
-    add_empty_id(form.created_by_id.choices)
     add_empty_id(form.closed_by_id.choices)
     add_empty_id(form.assign_to_id.choices)
 
@@ -166,7 +162,7 @@ def dashboard():
             return redirect(url_for('dashboard'))
 
         crm_identifier = req_to_be_approved.crm_app_no
-        opp_to_be_changed = Opportunity.query.filter_by(crm_app_no=crm_identifier).first()
+        opp_to_be_changed = Opportunity.query.filter_by(CRM_Appln_No=crm_identifier).first()
         if not opp_to_be_changed:
             flash('There is no opportunity with the specified CRM Application number.')
             return redirect(url_for('dashboard'))
@@ -209,21 +205,83 @@ def opportunity():
     class OppTable(Table):
         classes = ['table', 'table-bordered', 'table-hover']
 
-        crm_app_no = Col('crm_app_no')
-        fna_no = Col('fna_no')
+        CRM_Appln_No = Col('CRM_Appln_No')
+        id = Col('id')
 
-        created_by_name = Col('created_by_name')
-        created_by_id = Col('created_by_id')
-        create_date = Col('create_date')
+        Cust_Indicator = Col('Cust_Indicator')
+        Fullname = Col('Fullname')
+        ID_No = Col('ID_No')
 
-        closed_by_name = Col('closed_by_name')
-        closed_by_id = Col('closed_by_id')
-        close_date = Col('close_date')
+        Created_by_name = Col('Created_by_name')
+        Created_by_ID = Col('Created_by_ID')
+        Create_date = Col('Create_date')
 
-        assign_to_name = Col('assign_to_name')
-        assign_to_id = Col('assign_to_id')
+        Closed_by_name = Col('Closed_by_name')
+        Closed_by_ID = Col('Closed_by_ID')
+        Close_date = Col('Close_date')
 
-        pdt_name = Col('pdt_name')
+        Assign_to_name = Col('Assign_to_name')
+        Assign_to_ID = Col('Assign_to_ID')
+
+        Insurance_Ref = Col('Insurance_Ref')
+
+        Product_Name_L1 = Col('Product_Name_L1')
+        Product_Name_L2 = Col('Product_Name_L2')
+        Product_Name_L3 = Col('Product_Name_L3')
+        Product_Code = Col('Product_Code')
+
+        SalesType = Col('SalesType')
+        RefNo_Caption = Col('RefNo_Caption')
+        Base_Curr_Caption = Col('Base_Curr_Caption')
+        Alt_Curr_Caption = Col('Alt_Curr_Caption')
+        Opty_Amt_Caption = Col('Opty_Amt_Caption')
+        Opty_Amt = Col('Opty_Amt')
+        OD_Facility_Amt = Col('OD_Facility_Amt')
+
+        Currency_code = Col('Currency_code')
+        AltCurrency_code = Col('AltCurrency_code')
+
+        Opty_Source = Col('Opty_Source')
+        Opty_Type = Col('Opty_Type')
+        Referral_Code = Col('Referral_Code')
+        Referral_Comment = Col('Referral_Comment')
+
+        Match = Col('Match')
+        Acct_No = Col('Acct_No')
+        Acct_open_date = Col('Acct_open_date')
+        Match_Amt = Col('Match_Amt')
+
+        Decline = Col('Decline')
+        Decline_by_ID = Col('Decline_by_ID')
+        Decline_by_name = Col('Decline_by_name')
+        Decline_date = Col('Decline_date')
+        Decline_Reason = Col('Decline_Reason')
+
+        Assign_by_ID = Col('Assign_by_ID')
+        Assign_by_dt = Col('Assign_by_dt')
+
+        rp_pymt_freq = Col('rp_pymt_freq')
+        FD_TENOR = Col('FD_TENOR')
+        WEIGHTED_MARGIN = Col('WEIGHTED_MARGIN')
+        PRE_RECEIVE = Col('PRE_RECEIVE')
+        INTERIM_REV_PTS = Col('INTERIM_REV_PTS')
+
+        Admin_Update = Col('Admin_Update')
+        Updated_by_name = Col('Updated_by_name')
+        Updated_by_ID = Col('Updated_by_ID')
+        Update_date = Col('Update_date')
+
+        Match_Dt = Col('Match_Dt')
+        Unmatch_Dt = Col('Unmatch_Dt')
+
+        RevShareTSO = Col('RevShareTSO')
+        RevShareRM = Col('RevShareRM')
+        INTERIM_REV_PTS_TSO = Col('INTERIM_REV_PTS_TSO')
+        RP_Term = Col('RP_Term')
+        FNA_No = Col('FNA_No')
+
+        Property_Status = Col('Property_Status')
+        Property_Type = Col('Property_Type')
 
     opp_table = OppTable(opp)
     return render_template('opportunity.html', title='Opportunity Database', opp_table=opp_table)
